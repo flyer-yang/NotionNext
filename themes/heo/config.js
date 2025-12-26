09a030a4ff0d0c31c92211a501b3ec6795114353
flyer-yang <62193052+flyer-yang@users.noreply.github.com>
2025-12-26 18:32:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,9 +19,9 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_1: '让我们一起',
+  HEO_HERO_TITLE_2: '边走边赚',
+  HEO_HERO_TITLE_3: 'blog.yysky2020.xyz',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
