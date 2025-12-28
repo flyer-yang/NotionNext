b391a84f33c46930f01924a1a7d5ecc6cfc53598
flyer-yang <62193052+flyer-yang@users.noreply.github.com>
2025-12-28 10:23:25 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -58,60 +58,60 @@ const CONFIG = {
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
-      color_2: '#ffffff'
-    },
-    {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-      color_2: '#4082c3'
-    },
-    {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      title_1: 'Chase',
+      img_1: '/images/heo/Chase Mobile_ Bank & Invest-iOS-512x512.webp',
       color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      title_2: 'AmEx',
+      img_2: '/images/heo/Amex-iOS-512x512.webp',
       color_2: '#ffffff'
     },
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
+      title_1: 'CapitalOne',
+      img_1: '/images/heo/Capital One Mobile-iOS-512x512.webp',
+      color_1: '#ffffff',
+      title_2: 'HSBC',
+      img_2: '/images/heo/HSBC US-iOS-512x512.webp',
+      color_2: '#ffffff'
     },
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
-      color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
-      color_2: '#2c51db'
+      title_1: 'IBKR',
+      img_1: '/images/heo/IBKR Mobile - Invest Worldwide-iOS-512x512.webp',
+      color_1: '#ffffff',
+      title_2: 'Fidelity',
+      img_2: '/images/heo/Fidelity Investments-iOS-512x512.webp',
+      color_2: '#ffffff'
     },
     {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
-      color_2: '#e9572b'
+      title_1: 'ChatGPT',
+      img_1: '/images/heo/ChatGPT-iOS-512x512.webp',
+      color_1: '#ffffff',
+      title_2: 'Gemini',
+      img_2: '/images/heo/Google Gemini-iOS-512x512.webp',
+      color_2: '#ffffff'
     },
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
-      color_2: '#1f1f1f'
+      title_1: 'Notion',
+      img_1: '/images/heo/Notion_ Notes, Tasks, AI-iOS-512x512.webp',
+      color_1: '#ffffff',
+      title_2: 'Sora',
+      img_2: '/images/heo/Sora by OpenAI-iOS-512x512.webp',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Marriott',
+      img_1: '/images/heo/Marriott Bonvoy_ Book Hotels-iOS-512x512.webp',
+      color_1: '#ffffff',
+      title_2: 'IHG',
+      img_2: '/images/heo/IHG One Rewards_ Book Hotels-iOS-512x512.webp',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Hyatt',
+      img_1: '/images/heo/World of Hyatt_ Book Hotels-iOS-512x512.webp',
+      color_1: '#ffffff',
+      title_2: 'Hilton',
+      img_2: '/images/heo/Hilton Honors_ Book Hotels-iOS-512x512.webp',
+      color_2: '#ffffff'
     }
   ],
 
